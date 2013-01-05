bb998109e999e105fdacbedb6bb7b0435fd6b88e
think-free <meumeu1402@gmail.com>
2013-01-05 13:56:43 +0100

    Update wviewCompile.sh

--- wviewCompile.sh
+++ wviewCompile.sh
@@ -12,6 +12,39 @@
 # *
 # ****************************************************************** */
 
+######################################################################################
+# New version
+######################################################################################
+
+sudo apt-get install --yes build-essential zlib1g-dev libpng12-dev libreadline-dev gawk libsqlite3-dev sqlite3 libgd2-xpm libgd2-xpm-dev libssl-dev sendmail sendemail libcurl4-openssl-dev libusb-1.0-0 libusb-1.0-0-dev apache2 php5 php5-sqlite libapache2-mod-php5 ntp tnftp wget
+
+wget http://downloads.sourceforge.net/project/radlib/radlib/radlib-2.12.0/radlib-2.12.0.tar.gz
+
+tar zxvf radlib-2.12.0.tar.gz
+cd radlib-2.12.0
+./configure --enable-sqlite --prefix=/ --bindir=/usr/bin --sysconfdir=/etc --localstatedir=/var/lib --libdir=/usr/lib
+sudo make install
+cd ..
+
+wget http://garr.dl.sourceforge.net/project/wview/wview/wview-5.20.2/wview-5.20.2.tar.gz
+tar zxvf wview-5.20.2.tar.gz
+cd wview-5.20.2
+./configure --prefix=/ --bindir=/usr/bin --sysconfdir=/etc --localstatedir=/var/lib --libdir=/usr/lib
+sudo make install
+cd ..
+
+sudo wget --no-check-certificate https://raw.github.com/think-free/pi-scripts/master/wview-init-script -O /etc/init.d/wview
+sudo chmod +x /etc/init.d/wview
+sudo update-rc.d wview defaults 99
+sudo ln -s /var/lib/wview/img/ /var/www/weather
+sudo ln -s /var/lib/wviewmgmt/ /var/www/wviewmgmt
+
+exit 0
+
+######################################################################################
+# Older version
+######################################################################################
+
 # Getting dependencies
 
 sudo apt-get update
