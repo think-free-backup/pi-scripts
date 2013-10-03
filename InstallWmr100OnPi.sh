2ce9ec2b634095c1b5833c98710871271a9ff522
think-free <meumeu1402@gmail.com>
2013-10-03 13:14:57 +0200

    Update and rename InstallWmr100DepepdsOnPi.sh to InstallWmr100OnPi.sh

--- InstallWmr100OnPi.sh
+++ InstallWmr100OnPi.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-sudo apt-get install libusb-dev libsqlite3-dev
+sudo apt-get install libusb-dev
 wget --no-check-certificate https://github.com/think-free/pi-packages/raw/master/libhid-0.2.16-rpi.tar.gz
 tar xvf libhid-0.2.16-rpi.tar.gz
 cd libhid-0.2.16
