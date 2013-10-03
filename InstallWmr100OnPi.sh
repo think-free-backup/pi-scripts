f26f267553f88b5ac2b4dab80de07c6b063192ff
think-free <meumeu1402@gmail.com>
2013-10-03 13:22:44 +0200

    Update InstallWmr100OnPi.sh

--- InstallWmr100OnPi.sh
+++ InstallWmr100OnPi.sh
@@ -1,9 +1,14 @@
 #!/bin/bash
 
-sudo apt-get install libusb-dev
+sudo apt-get install libusb-dev libzmq-dev
 wget --no-check-certificate https://github.com/think-free/pi-packages/raw/master/libhid-0.2.16-rpi.tar.gz
 tar xvf libhid-0.2.16-rpi.tar.gz
 cd libhid-0.2.16
 ./configure --prefix=/
 make
 sudo make install
+cd ..
+
+git clone https://github.com/barnybug/wmr100.git
+cd wmr100
+make
