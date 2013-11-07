595f693f940cb3a3dc0df2617353f571e01c2ab8
think-free <meumeu1402@gmail.com>
2013-11-07 13:53:16 +0100

    Update cloud.sh

--- cloud.sh
+++ cloud.sh
@@ -4,32 +4,77 @@
 # http://distribution.hexxeh.net/raspbian/raspbian-r3.zip
 # user : root - password : hexxeh
 
-echo "Please, change the default password (root:hexxeh)"
+if [ "$#" -ne 1 ];
+then
 
-echo "Preparing sdcard"
+  echo "Please, change the default password (root:hexxeh)"
+  
+  echo "Preparing sdcard"
+  
+  echo "vm.min_free_kbytes = 8192" >> /etc/sysctl.conf
+  sysctl -p /etc/sysctl.conf
+  rm /etc/ssh/ssh_host_* && dpkg-reconfigure openssh-server
+  apt-get update
+  apt-get install ntp fake-hwclock vim
+  dpkg-reconfigure tzdata
+  apt-get install locales
+  dpkg-reconfigure locales
+  apt-get install console-setup keyboard-configuration
+  dpkg-reconfigure keyboard-configuration
+  apt-get update && apt-get dist-upgrade
+  apt-get install ntpdate
+  ntpdate uk.pool.ntp.org
+  rpi-update
+fi
 
-echo "vm.min_free_kbytes = 8192" >> /etc/sysctl.conf
-sysctl -p /etc/sysctl.conf
-rm /etc/ssh/ssh_host_* && dpkg-reconfigure openssh-server
-apt-get update
-apt-get install ntp fake-hwclock vim
-dpkg-reconfigure tzdata
-apt-get install locales
-dpkg-reconfigure locales
-apt-get install console-setup keyboard-configuration
-dpkg-reconfigure keyboard-configuration
-apt-get update && apt-get dist-upgrade
-apt-get install ntpdate
-ntpdate uk.pool.ntp.org
-rpi-update
+if [ "$1" == "database" ];
+then
 
-echo "Installing database"
+	echo "Installing database"
 
-apt-get install couchdb
-sed -i 's|127.0.0.1|0.0.0.0|' /etc/couchdb/default.ini
-/etc/init.d/couchdb restart
+	apt-get install couchdb
+	sed -i 's|127.0.0.1|0.0.0.0|' /etc/couchdb/default.ini
+	/etc/init.d/couchdb restart
 
-echo "Installing nodejs"
+	# TODO : Install zeroconf script
+fi
 
-wget https://github.com/think-free/pi-packages/raw/master/nodejs_0.8.15-1_armhf.deb --no-check-certificate
-sudo dpkg -i nodejs_0.8.15-1_armhf.de
+if [ "$1" == "wmr100" ];
+then
+
+	installNode
+	
+	echo "Installing wmr100"
+
+	sudo apt-get install libusb-dev libzmq-dev
+	wget --no-check-certificate https://github.com/think-free/pi-packages/raw/master/libhid-0.2.16-rpi.tar.gz
+	tar xvf libhid-0.2.16-rpi.tar.gz
+	cd libhid-0.2.16
+	./configure --prefix=/
+	make
+	sudo make install
+	cd ..
+
+	git clone https://github.com/barnybug/wmr100.git
+	cd wmr100
+	make
+
+  wget --no-check-certificate https://raw.github.com/think-free/wmr100.js/master/wmr100.js
+
+	# TODO : Install zeroconf script
+fi
+
+
+
+
+# FUNCTIONS #################################################################################################
+
+function installNode {
+
+	echo "Installing nodejs"
+
+  wget https://github.com/think-free/pi-packages/raw/master/nodejs_0.8.15-1_armhf.deb --no-check-certificate
+  sudo dpkg -i nodejs_0.8.15-1_armhf.de
+}
+
+
